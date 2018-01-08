9c2f0e08b29eda6662ea856c8bd2e7ccca7934de
Jihoon Lee <jihoonlee.in@gmail.com>
2018-01-08 14:50:06 +0900

    fix

--- test/build_push.sh
+++ test/build_push.sh
@@ -8,15 +8,21 @@ setup_git() {
 checkout_and_commit() {
   git fetch origin ${TRAVIS_PULL_REQUEST_BRANCH}
   git checkout -b ${TRAVIS_PULL_REQUEST_BRANCH} origin/${TRAVIS_PULL_REQUEST_BRANCH}
-  echo "Add built module"
-  git add build/roslib.js
-  git add build/roslib.min.js
-  git commit build/roslib.js build/roslib.min.js -m "Update built module"
+
+
+
 }
 
 push() {
   echo "Add Remote"
   git remote add pr_origin https://${GITHUB_API_KEY}@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git > /dev/null 2>&1
+  git fetch pr_origin ${TRAVIS_PULL_REQUEST_BRANCH}
+  git checkout -b ${TRAVIS_PULL_REQUEST_BRANCH} pr_origin/${TRAVIS_PULL_REQUEST_BRANCH}
+  echo "Add built module and commit"
+  git add build/roslib.js
+  git add build/roslib.min.js
+  git commit build/roslib.js build/roslib.min.js -m "Update built module"
+
   echo "Push"
   git push --quiet --set-upstream pr_origin ${TRAVIS_PULL_REQUEST_BRANCH}_to_push:${TRAVIS_PULL_REQUEST_BRANCH}
 }
@@ -32,7 +38,6 @@ echo "PR SLUG : ${TRAVIS_PULL_REQUEST_SLUG}"
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo "Run push"
   setup_git
-  checkout_and_commit
   push
 fi
 
