2900f92d123550886760b0d2e51f8437b310be34
Travis CI <travis@travis-ci.org>
2018-01-08 06:04:35 +0000

    Update built module

--- build/roslib.js
+++ build/roslib.js
@@ -826,7 +826,7 @@ module.exports = shouldUseNative() ? Object.assign : function (target, source) {
  * If you use nodejs, this is the variable you get when you require('roslib')
  */
 var ROSLIB = this.ROSLIB || {
-  REVISION : '0.20.0'
+  REVISION : '0.20.0-test'
 };
 
 var assign = require('object-assign');
